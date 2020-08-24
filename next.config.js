e7c6a1392da46c26059bfae355631a9965815f7b
Walter Bonilla <wbonillacr@gmail.com>
2020-08-23 21:08:11 -0600

    Domain api server url

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
         MAILGUN_PRIVATE_KEY: 'key-8cb70886ea69a3dff5112e1d026d1057',
         MAILGUN_ADMIN_MAILING_LIST: 'info@notification.cabanadata.com',
         SANITY_PROJECT_ID: 'oxu53700',
-        API_SERVER_URL: 'http://localhost:3000'
+        API_SERVER_URL: 'https://clinicaodontovida.vercel.app',
     }
 }
