16b659fd3e4ebc95d9cb2cd6165cfc6edc92995a
Walter <walter@cabanadata.com>
2021-01-05 13:32:12 -0600

    Google Analytics

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -9,19 +9,33 @@ interface Props {
 export default class extends React.Component<Props> {
     render() {
         return (<>
-                <MainInfoContext.Consumer>
-                    {(value) => (
-                        <div id="page" className="page">
-                            {
-                                value.isLoaded && (<>
-                                    <Header />
-                                    {this.props.children}
-                                    <Footer  />
-                                </>)
-                            }
-                        </div>
-                    )}
-                </MainInfoContext.Consumer>
-            </>);
+            <MainInfoContext.Consumer>
+                {(value) => (
+                    <div id="page" className="page">
+                        {
+                            process.env.GTM_ID &&
+                            <>
+                                <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}></script>
+                                <script
+                                    dangerouslySetInnerHTML={{
+                                        __html: `window.dataLayer = window.dataLayer || [];
+                                                    function gtag(){dataLayer.push(arguments);}
+                                                    gtag('js', new Date());                                                  
+                                                    gtag('config', '${process.env.GTM_ID}');`,
+                                    }}
+                                />
+                            </>
+                        }
+                        {
+                            value.isLoaded && (<>
+                                <Header />
+                                {this.props.children}
+                                <Footer />
+                            </>)
+                        }
+                    </div>
+                )}
+            </MainInfoContext.Consumer>
+        </>);
     }
 }
--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
         MAILGUN_RECEIVER_EMAIL: 'wbonillacr@gmail.com',
         MAILGUN_ADMIN_MAILING_LIST: 'info@notification.cabanadata.com',
         SANITY_PROJECT_ID: 'oxu53700',
+        GTM_ID: 'G-3KFFE7S2T6',
         API_SERVER_URLPROD: 'https://clinicaodontovida.vercel.app',
         API_SERVER_URL: 'http://localhost:3000',
     }
